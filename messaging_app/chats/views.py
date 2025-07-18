38ba7bb46ca8bad3c780de1f240ffb54b8160ffc
Demarkus <demarkusmcnabb@gmail.com>
2025-07-18 12:01:44 -0700

    Add filters to viewsets and wire up API URLs

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 from django.shortcuts import render # type: ignore
 """Views for chats app."""
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet for listing, creating conversations.
-    """
     queryset = Conversation.objects.all().prefetch_related("participants", "messages")
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
 
     @action(detail=True, methods=['post'])
     def send_message(self, request, pk=None):
-        """
-        Custom endpoint to send a message to this conversation.
-        """
         conversation = self.get_object()
         data = request.data.copy()
         data['conversation'] = str(conversation.conversation_id)
@@ -30,9 +26,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class MessageViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet for listing, creating messages.
-    """
     queryset = Message.objects.all().select_related("conversation", "sender")
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['sender__username', 'conversation__conversation_id', 'message_body']
 
