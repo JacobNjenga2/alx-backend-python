71330c8973b1198320e9ef350a2f20133c9bf785
Demarkus <demarkusmcnabb@gmail.com>
2025-07-22 05:38:19 -0700

    "Implement JWT authentication and permissions and auth"

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -162,6 +162,9 @@ REST_FRAMEWORK = {
     ],
 
     # 📦 Pagination
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
