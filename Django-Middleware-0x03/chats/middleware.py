9e2d75e5289dc72f01d62bfdb75426d50919cc85
Demarkus <demarkusmcnabb@gmail.com>
2025-07-28 12:12:46 -0700

    Add RestrictAccessByTimeMiddleware and configure in settings

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -67,3 +67,14 @@ class RolepermissionMiddleware:
                 return HttpResponseForbidden('You do not have permission to perform this action.')
         return self.get_response(request)
 
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        # Block access between 12 AM (0) and 6 AM (5)
+        if 0 <= current_hour < 6:
+            return HttpResponseForbidden("Access is restricted between 12 AM and 6 AM.")
+        return self.get_response(request)
+
